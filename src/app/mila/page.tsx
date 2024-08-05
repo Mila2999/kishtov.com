663392ed5b9de0a5b59f10e5255f6a834d296b49
mila2999 <mila.savonik@gmail.com>
2024-08-05 15:49:38 +0300

    fixes

--- src/app/mila/page.tsx
+++ src/app/mila/page.tsx
@@ -4,7 +4,7 @@ import Stack from '@mui/material/Stack';
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
-import Blog from './blog';
+import Blog from './mila';
 export default function About() {
   return (
     <main className="flex bg-blue-100 min-h-screen flex-col items-center justify-between content-between p-2">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import Blog from './[blog]/blog';
-import Billing from './[andrey]/billing';
+import Blog from './mila/mila';
+import Billing from './andrey/billing';
 import Image from 'next/image';
 import Card from './card/card';
 import ImageList from './image-list/imageList';
@@ -7,8 +7,10 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between content-between p-2">
-      <Link href="/blog/blog"></Link>
-      <Link href="/andrey/billing"></Link>
+      <nav>
+        <Link href="/blog/blog"></Link>
+        <Link href="/andrey/billing"></Link>
+      </nav>
       <ImageList />
       {/* <div className="flex justify-between content-between w-100 flex-wrap">
         {Array(8)
