f590434595062e6c6f08420bdfe17b80db5eefa2
mila2999 <mila.savonik@gmail.com>
2024-08-17 19:25:52 +0300

    Added MilaAbout

--- src/app/mila/mila.tsx
+++ src/app/mila/mila.tsx
@@ -1,11 +1,37 @@
 'use client';
-const Blog = () => {
+import React from 'react';
+// import AboutBackground from '../Assets/about-background.png';
+// import AboutBackgroundImage from '../Assets/about-background-image.png';
+import { BsFillPlayCircleFill } from 'react-icons/bs';
+const MilaAbout: () => React.JSX.Element = () => {
   return (
-    <div>
-      <h1>About Us</h1>
-      <p>This is the page about us.</p>
+    <div className="mt-80 relative flex items-center justify-between">
+      <div className="absolute left-[-150px] z-[-2]">
+        {/* <img src={AboutBackground} alt="" /> */}
+      </div>
+      <div className="flex-[0.9] mr-4">{/* <img src={AboutBackgroundImage} alt="" /> */}</div>
+      <div className="flex-1 flex justify-center flex-col">
+        <p className="primary-subheading">About</p>
+        <h1 className="primary-heading">
+          Digital Tools Are An Essential Part Of A Balanced Business Strategy
+        </h1>
+        <p className="primary-text">
+          Lorem ipsum dolor sit amet consectetur. Non tincidunt magna non et elit. Dolor turpis
+          molestie dui magnis facilisis at fringilla quam.
+        </p>
+        <p className="primary-text">
+          Non tincidunt magna non et elit. Dolor turpis molestie dui magnis facilisis at fringilla
+          quam.
+        </p>
+        <div className="about-buttons-container">
+          <button className="secondary-button">Learn More</button>
+          <button className="watch-video-button">
+            <BsFillPlayCircleFill /> Watch Video
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default Blog;
+export default MilaAbout;
--- src/app/mila/page.tsx
+++ src/app/mila/page.tsx
@@ -4,12 +4,12 @@ import Stack from '@mui/material/Stack';
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
-import Blog from './mila';
+import MilaAbout from './mila';
 export default function About() {
   return (
     <main className="flex bg-blue-100 min-h-screen flex-col items-center justify-between content-between p-2">
       <Container fixed>
-        <Blog />
+        <MilaAbout />
       </Container>
     </main>
   );
